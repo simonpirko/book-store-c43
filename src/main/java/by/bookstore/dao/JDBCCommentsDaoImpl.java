95748c2817389e7e0f6afefa2bf5fe83cd0a9d0f
shenja88 <voluevich88@gmail.com>
2021-09-19 18:14:37 +0300

    refractoring CommentsDao

--- src/main/java/by/bookstore/dao/JDBCCommentsDaoImpl.java
+++ src/main/java/by/bookstore/dao/JDBCCommentsDaoImpl.java
@@ -2,41 +2,44 @@ package by.bookstore.dao;
 
 import by.bookstore.entity.Book;
 import by.bookstore.entity.Comment;
+import by.bookstore.entity.TypeOfUser;
 import by.bookstore.entity.User;
 import by.bookstore.utils.MySQLConnection;
 
 import java.sql.*;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 public class JDBCCommentsDaoImpl implements CommentsDao {
+
     @Override
     public boolean save(Comment comment) {
-        try(Connection connection = MySQLConnection.getConnection()) {
+        try (Connection connection = MySQLConnection.getConnection()) {
             String sqlSaveComment = "INSERT INTO comments VALUES (default,?,?,?,?)";
             PreparedStatement statement = connection.prepareStatement(sqlSaveComment);
-            statement.setObject(1,comment.getTime());
-            statement.setLong(2,comment.getUser().getId());
-            statement.setString(3,comment.getDescription());
-            statement.setLong(4,comment.getBook().getId());
+            statement.setObject(1, comment.getTime());
+            statement.setLong(2, comment.getUser().getId());
+            statement.setString(3, comment.getDescription());
+            statement.setLong(4, comment.getBook().getId());
             return statement.execute();
         } catch (SQLException throwables) {
             throwables.printStackTrace();
-        }return false;
+        }
+        return false;
     }
 
     @Override
     public boolean deleteById(long commentId) {
-        try(Connection connection = MySQLConnection.getConnection()) {
+        try (Connection connection = MySQLConnection.getConnection()) {
             String sqlDeleteComment = "DELETE FROM comments WHERE id = ?";
             PreparedStatement statement = connection.prepareStatement(sqlDeleteComment);
             statement.setLong(1, commentId);
             return statement.execute();
         } catch (SQLException throwables) {
             throwables.printStackTrace();
-        }return false;
+        }
+        return false;
     }
 
     @Override
@@ -66,53 +69,64 @@ public class JDBCCommentsDaoImpl implements CommentsDao {
 
     @Override
     public boolean isExistByInfo(Comment comment) {
-        try(Connection connection = MySQLConnection.getConnection()){
+        try (Connection connection = MySQLConnection.getConnection()) {
             String isExistById = "SELECT * FROM comments WHERE (user_id = ? && book_id = ? && description = ?)";
             PreparedStatement preparedStatement = connection.prepareStatement(isExistById);
-            preparedStatement.setLong(1,comment.getUser().getId());
-            preparedStatement.setLong(2,comment.getBook().getId());
-            preparedStatement.setString(3,comment.getDescription());
+            preparedStatement.setLong(1, comment.getUser().getId());
+            preparedStatement.setLong(2, comment.getBook().getId());
+            preparedStatement.setString(3, comment.getDescription());
             ResultSet resultSet = preparedStatement.executeQuery(isExistById);
             return resultSet.next();
-        }catch (SQLException throwables){
+        } catch (SQLException throwables) {
             throwables.printStackTrace();
-        }return false;
+        }
+        return false;
     }
 
     @Override
     public boolean isExistById(long commentId) {
-        try(Connection connection = MySQLConnection.getConnection()){
+        try (Connection connection = MySQLConnection.getConnection()) {
             String isExistById = "SELECT * FROM comments WHERE id = ?";
             PreparedStatement preparedStatement = connection.prepareStatement(isExistById);
-            preparedStatement.setLong(1,commentId);
-            ResultSet resultSet = preparedStatement.executeQuery(isExistById);
+            preparedStatement.setLong(1, commentId);
+            ResultSet resultSet = preparedStatement.executeQuery();
             return resultSet.next();
-        }catch (SQLException throwables){
+        } catch (SQLException throwables) {
             throwables.printStackTrace();
-        }return false;
+        }
+        return false;
     }
 
     @Override
     public List<Comment> getAllByBookIdSortByDate(long bookId) {
         List<Comment> comments = new ArrayList<>();
-        try(Connection connection = MySQLConnection.getConnection()){
-            String isExistById = "SELECT * FROM comments WHERE book_id = ?";
+        try (Connection connection = MySQLConnection.getConnection()) {
+            String isExistById = "SELECT comments.id AS comments_id, comments.time, comments.description, " +
+                    " b.id AS book_id, b.name, b.author, u.id AS user_id, u.name, u.picture, u.typeOfUser" +
+                    " FROM comments JOIN books b on b.id = comments.book_id" +
+                    " JOIN users u on u.id = comments.user_id WHERE b.id = ?";
             PreparedStatement preparedStatement = connection.prepareStatement(isExistById);
-            preparedStatement.setLong(1,bookId);
-            ResultSet resultSet = preparedStatement.executeQuery(isExistById);
-            while (resultSet.next()){
+            preparedStatement.setLong(1, bookId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
                 comments.add(new Comment(
-                        resultSet.getLong("id"),
+                        resultSet.getLong("comments_id"),
                         new Date(resultSet.getTime("time").getTime()).toLocalDate().atStartOfDay(), //Задействован один из способов приведения типа данных Date -> LocalDate, требуется тестировние с текущими настройками БД.
-                        new User(resultSet.getInt("user_id")),
+                        new User(resultSet.getInt("user_id"),
+                                resultSet.getString("name"),
+                                resultSet.getString("picture"),
+                                TypeOfUser.valueOf(resultSet.getString("typeOfUser"))
+                        ),
                         resultSet.getString("description"),
-                        new Book(resultSet.getInt("book_id"))
+                        new Book(resultSet.getInt("book_id"),
+                                resultSet.getString("name"),
+                                resultSet.getString("author"))
                 ));
             }
             comments.sort(Comparator.comparing(Comment::getTime));
-            return comments;
-        }catch (SQLException throwables){
+        } catch (SQLException throwables) {
             throwables.printStackTrace();
-        }return comments;
+        }
+        return comments;
     }
 }
