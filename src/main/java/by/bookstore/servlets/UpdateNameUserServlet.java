11618107a285b328c6aafb05e6eda5b45b061bed
SheidakMaryia <mashasheidak@mail.ru>
2021-09-23 21:40:04 +0300

    Add UpdateNameUserServlet

---
+++ src/main/java/by/bookstore/servlets/UpdateNameUserServlet.java
@@ -0,0 +1,36 @@
+package by.bookstore.servlets;
+
+import by.bookstore.entity.User;
+import by.bookstore.service.FacadeService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "UpdateNameUserServlet", urlPatterns = "/updateNameUser")
+public class UpdateNameUserServlet extends HttpServlet {
+
+    private final FacadeService facadeService = new FacadeService();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getServletContext().getRequestDispatcher("").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        User user = (User) req.getSession().getAttribute("user");
+        String newName = req.getParameter("newName");
+
+        if (facadeService.changeName(user, newName)){
+            req.setAttribute("message_upd_n", "Successfully update name");
+        }else {
+            req.setAttribute("message_upd_n", "Unsuccessfully update name");
+        }
+        req.getServletContext().getRequestDispatcher("").forward(req, resp);
+
+    }
+}
