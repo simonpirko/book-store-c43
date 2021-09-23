1683da2827db212851efe9cce31950710a0111b3
Evgeniy2312 <evgen.matvee1986@gmail.com>
2021-09-23 17:07:54 +0300

    add servlets(BookStoreServlet, UserBookServlet)

---
+++ src/main/java/by/bookstore/servlets/BookStoreServlet.java
@@ -0,0 +1,22 @@
+package by.bookstore.servlets;
+
+import by.bookstore.service.FacadeService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet
+public class BookStoreServlet extends HttpServlet {
+
+    private final FacadeService facadeService = new FacadeService();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getSession().setAttribute("allBooks", facadeService.getBooks());
+        req.getServletContext().getRequestDispatcher("").forward(req, resp);
+    }
+}
---
+++ src/main/java/by/bookstore/servlets/UserBookServlet.java
@@ -0,0 +1,23 @@
+package by.bookstore.servlets;
+
+import by.bookstore.entity.User;
+import by.bookstore.service.FacadeService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet
+public class UserBookServlet extends HttpServlet   {
+    private final FacadeService facadeService = new FacadeService();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        User user = (User) req.getSession().getAttribute("user");
+        req.getSession().setAttribute("userBooks", facadeService.getBooksById(user.getId()));
+        req.getServletContext().getRequestDispatcher("").forward(req, resp);
+    }
+}
